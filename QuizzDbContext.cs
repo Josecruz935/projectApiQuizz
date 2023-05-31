f2fa275bbe4f99eb36df68c60f2fbc661c300b38
Daniel Gregorio Reyes Alvarez <daniel.reyes@grupocorinsa.hn>
2023-05-31 09:03:30 -0600

    Agregando DbContext

---
+++ QuizzDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace apiQuizz.Models
+{
+    public class QuizzDbContext : DbContext
+    {
+        public QuizzDbContext(DbContextOptions<QuizzDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+    }
+}
